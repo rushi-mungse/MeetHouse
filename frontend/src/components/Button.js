88dffa939eb4560d69c2a9e772dc09ab570f70d5
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-04-04 22:28:46 +0530

    propstype and defaultprops

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,3 +1,4 @@
+import PropsType from "prop-types";
 const Button = ({ text, img }) => {
   return (
     <button className="bg-orange-500 py-1 px-4 flex justify-center items-center rounded-full hover:bg-orange-600 duration-300 ease-in-out">
@@ -7,4 +8,13 @@ const Button = ({ text, img }) => {
   );
 };
 
+Button.prototype = {
+  text: PropsType.string,
+  img: PropsType.string,
+};
+
+Button.defaultProps = {
+  text: "Next",
+  img: "arrow-forward",
+};
 export default Button;
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,12 +1,11 @@
-const Card = ({ children }) => {
+import PropsType from "prop-types";
+const Card = ({ heading, children }) => {
   return (
     <div>
       <div className="bg-gray-100 w-11/12 sm:w-11/12 md:w-2/3 lg:w-1/2   px-8 py-12  flex justify-center items-center flex-col rounded-lg mx-auto">
         <div className="flex items-center justify-center">
           <img src="/images/logo.png" alt="logo" />
-          <h1 className="text-2xl sm:text-3xl font-bold ml-4">
-            Welcome to MeetHouse!
-          </h1>
+          <h1 className="text-2xl sm:text-3xl font-bold ml-4">{heading}</h1>
         </div>
         {children}
       </div>
@@ -14,4 +13,10 @@ const Card = ({ children }) => {
   );
 };
 
+Card.prototype = {
+  heading: PropsType.srting,
+};
+Card.defaultProps = {
+  heading: "Welcome to MeetHouse",
+};
 export default Card;
