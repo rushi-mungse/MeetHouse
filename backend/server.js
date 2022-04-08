701595b9e27b752574b106bf3099b129bc64960b
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-04-08 19:41:53 +0530

    cors setup

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ import { APP_PORT } from "./config";
 import { handleErrors } from "./middlewares";
 import router from "./routes";
 import dbConnect from "./utils/database";
+import cors from "cors";
 
 const app = express();
+
+const options = {
+  origin: "http:localhost:3000",
+};
+app.use(cors(options));
 
 app.use(express.json());
 app.use("/api", router);
 app.use(handleErrors);
+
 dbConnect();
+
 const port = APP_PORT || 5500;
 app.listen(port, console.log(`Server listening on port ${port}`));
